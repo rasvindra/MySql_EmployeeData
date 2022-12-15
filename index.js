8e2410dae813d8243ef165c402c48e2b54a1a2d7
Ravi Nagi <rasvindra@hotmail.com>
2022-12-15 12:45:15 -0800

    added choices for user to manipulate data, still need to write the functions

--- index.js
+++ index.js
@@ -10,7 +10,47 @@ const connection = mysql.createConnection({
     database: "employee_data", 
 });
 
+const init = () => {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "choices",
+            message: "Please select the Option that best suits your Needs!",
+            choices: 
+            [
+                "Show all Departments",
+                "Show all Employee Job roles",
+                "Review all Employee Data",
+                "Add a new Department to the Company",
+                "Add a New Employee to the Company",
+                "Add a New Job role for an Employee",
+                "Update the Job Role for an Employee"
+            ],
+        },
+    ]).then ((res) => {
+        switch (res.choices) {
+            case "Show all Departments": showallDept();
+            break;
+            case "Show all Employee Job roles": showemployeeRoles();
+            break;
+            case "Review all Employee Data": reviewEmployeeData();
+            break;
+            case "Add a new Department to the Company": addDepartment();
+            break;
+            case "Add a New Employee to the Company": addEmployee();
+            break;
+            case "Add a New Job role for an Employee": addNewJobrole();
+            break;
+            case "Update the Job Role for an Employee": updateJobrole();
+            break;
+            default: throw new Error("We Apologize. Something Went Wrong. Please Retry");
+            
+        }
+    });
+};
+
 connection.connect((err) => {
     if (err) throw err;
     console.table("Thank you for Choosing our Application to help the Needs of your Company!");
+    init();
 });
